80d14261d402d1ac712ecf8ea426ffb9e9ff9081
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-04 17:11:32 +0200

    Highlight target cell when dragging ship

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 const Cell = ({
   playerName,
   shipNames,
@@ -7,20 +9,40 @@ const Cell = ({
   handleClickCell,
   handlePlaceShip,
 }) => {
-  const bgColor =
-    shipNames.includes(content) && playerName === "Player"
-      ? "grey"
-      : content === "miss"
-      ? "red"
-      : content === "hit"
-      ? "green"
-      : "white";
+  const [bgColor, setBgColor] = useState("white");
+
+  useEffect(() => {
+    if (shipNames.includes(content) && playerName === "Player") {
+      setBgColor("gray");
+    } else if (content === "miss") {
+      setBgColor("red");
+    } else if (content === "hit") {
+      setBgColor("green");
+    } else {
+      setBgColor("white");
+    }
+  }, [content]);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setBgColor("gray");
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    if (shipNames.includes(content)) {
+      setBgColor("gray");
+    } else {
+      setBgColor("white");
+    }
+  };
 
   return (
     <button
       onClick={handleClickCell ? () => handleClickCell(x, y) : null}
       onDrag={handlePlaceShip ? (e) => e.preventDefault() : null}
-      onDragOver={(e) => e.preventDefault()}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
       onDrop={handlePlaceShip ? (e) => handlePlaceShip(e) : null}
       data-shipname={content}
       key={`${x}-${y}`}
