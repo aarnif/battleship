558c88cb6488a68b2097bdb53fd2cb9f2ec454d5
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-08 19:01:48 +0200

    Add Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="p-8 w-full flex justify-center items-center bg-header">
+      <div className="flex-grow flex justify-center items-center">
+        <h1 className="text-6xl font-extrabold text-headerText">Battleship</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
