e720af73014b0aa2730c055eb6a4777a1631f6e9
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-11 18:16:58 +0200

    Use tailwind for animation in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import { FaGithub } from "react-icons/fa";
-import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
@@ -9,13 +8,7 @@ const Footer = () => {
           <h3 className="text-xl font-bold">Created By aarnif</h3>
         </li>
         <li className="px-1">
-          <motion.div
-            whileHover={{
-              scale: 1.5,
-              rotate: 360,
-            }}
-            transition={{ duration: 0.5 }}
-          >
+          <div className="hover:animate-rotate-and-scale">
             <a
               href="https://github.com/aarnif"
               target="_blank"
@@ -23,7 +16,7 @@ const Footer = () => {
             >
               <FaGithub className="w-6 h-6 fill-current" />
             </a>
-          </motion.div>
+          </div>
         </li>
       </ul>
     </footer>
