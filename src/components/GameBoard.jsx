cedd13a5546a252be83f654df2920d68ab19fcc2
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-26 17:05:12 +0200

    Add ship icons to game board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import ShipIcon from "./ShipIcon.jsx";
+import helpers from "../helpers.js";
 
 const Cell = ({ playerName, shipNames, content, y, x, handleClickCell }) => {
   const classStyles = {
     default:
       "w-12 h-12 bg-cell hover:bg-cell-hover border border-border group cursor-pointer",
-    ship: "w-12 h-12 bg-shipCell border border-border group cursor-pointer",
+    ship: "w-12 h-12 bg-ship-cell border border-border group cursor-pointer",
     miss: "w-12 h-12 bg-cell-miss border border-border group cursor-default",
     hit: "w-12 h-12 bg-cell-hit border border-border group cursor-default",
   };
@@ -48,9 +50,22 @@ const GameBoard = ({
 }) => {
   const shipNames = ships.map((ship) => ship.name);
 
+  const [gridTemplateAreas, setGridTemplateAreas] = useState(
+    `'${helpers.generateGridTemplate(shipNames, gameBoard)}'`
+  );
+
+  const [nonShipCells, setNonShipCells] = useState(
+    helpers.generateNonShipCells(shipNames, gameBoard)
+  );
+
   const boardContainerClass = {
     Player: "z-10 relative flex flex-col items-center",
     Computer: "relative flex flex-col items-center",
+  };
+
+  const shipClasses = {
+    horizontal: "relative group flex justify-center items-center",
+    vertical: "relative group flex flex-col justify-center items-center",
   };
 
   return (
@@ -63,23 +78,46 @@ const GameBoard = ({
       <div
         className="grid"
         style={{
-          gridTemplateColumns: `repeat(${gameBoard.length}, 1fr)`,
-          gridTemplateRows: `repeat(${gameBoard.length}, 1fr)`,
+          gridTemplateAreas: gridTemplateAreas,
         }}
       >
-        {gameBoard.map((row, x) =>
-          row.map((cell, y) => (
-            <Cell
-              key={`${x}-${y}`}
-              playerName={playerName}
-              shipNames={shipNames}
-              x={x}
-              y={y}
-              content={cell}
-              handleClickCell={handleClickCell}
-            />
-          ))
-        )}
+        {ships.map((ship) => (
+          <motion.div
+            key={ship.name}
+            style={{
+              gridArea: ship.name,
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className={shipClasses[ship.position]}
+          >
+            {(ship.isSunk || playerName === "Player") && (
+              <ShipIcon ship={ship} />
+            )}
+            {ship.coordinates.map((coordinate) => (
+              <Cell
+                key={`${coordinate[0]}-${coordinate[1]}`}
+                playerName={playerName}
+                shipNames={shipNames}
+                x={coordinate[0]}
+                y={coordinate[1]}
+                content={gameBoard[coordinate[0]][coordinate[1]]}
+                handleClickCell={handleClickCell}
+              />
+            ))}
+          </motion.div>
+        ))}
+        {nonShipCells.map((cell) => (
+          <Cell
+            key={`${cell[0]}-${cell[1]}`}
+            playerName={playerName}
+            shipNames={shipNames}
+            x={cell[0]}
+            y={cell[1]}
+            content={gameBoard[cell[0]][cell[1]]}
+            handleClickCell={handleClickCell}
+          />
+        ))}
       </div>
     </motion.div>
   );
