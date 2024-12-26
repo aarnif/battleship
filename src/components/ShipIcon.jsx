98e2ccd864658b5ff1a70b46ca5f9426980bd751
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-26 17:03:19 +0200

    Add styling for sunk ship in ShipIcon.jsx

--- src/components/ShipIcon.jsx
+++ src/components/ShipIcon.jsx
@@ -4,47 +4,80 @@ import Cruiser from "./Cruiser";
 import Submarine from "./Submarine";
 import Destroyer from "./Destroyer";
 
-const shipIcons = {
-  Carrier: {
-    horizontal: "absolute cursor-pointer w-60 h-12 pointer-events-none",
-    vertical: "absolute rotate-90 cursor-pointer w-60 h-12 pointer-events-none",
-    component: (
-      <Carrier className="w-full h-full object-cover fill-current text-ship-icon" />
-    ),
-  },
-  Battleship: {
-    horizontal:
-      "absolute text-ship-icon cursor-pointer w-full h-12 pointer-events-none",
-    vertical:
-      "absolute text-ship-icon rotate-90 cursor-pointer w-48 h-12 pointer-events-none",
-    component: (
-      <Battleship className="w-full h-full object-cover fill-current text-ship-icon" />
-    ),
-  },
-  Cruiser: {
-    horizontal: "absolute cursor-pointer w-full h-12 pointer-events-none",
-    vertical: "absolute rotate-90 cursor-pointer w-36 h-12 pointer-events-none",
-    component: (
-      <Cruiser className="w-full h-full object-cover fill-current text-ship-icon" />
-    ),
-  },
-  Submarine: {
-    horizontal: "absolute cursor-pointer w-full h-12 pointer-events-none",
-    vertical: "absolute rotate-90 cursor-pointer w-36 h-12 pointer-events-none",
-    component: (
-      <Submarine className="w-full h-full object-cover fill-current text-ship-icon" />
-    ),
-  },
-  Destroyer: {
-    horizontal: "absolute cursor-pointer w-full h-12 pointer-events-none",
-    vertical: "absolute rotate-90 cursor-pointer w-24 h-12 pointer-events-none",
-    component: (
-      <Destroyer className="w-full h-full object-cover fill-current text-ship-icon" />
-    ),
-  },
-};
-
 const ShipIcon = ({ ship, position = ship.position }) => {
+  const shipIconStyles = {
+    isFloat: "w-full h-full object-cover fill-current text-ship-icon",
+    isSunk: "w-full h-full object-cover fill-current text-ship-icon-sunk",
+  };
+
+  const shipIcons = {
+    Carrier: {
+      horizontal:
+        "absolute cursor-pointer w-60 h-12 pointer-events-none animate-fade-in",
+      vertical:
+        "absolute rotate-90 cursor-pointer w-60 h-12 pointer-events-none animate-fade-in",
+      component: (
+        <Carrier
+          className={
+            ship.isSunk ? shipIconStyles.isSunk : shipIconStyles.isFloat
+          }
+        />
+      ),
+    },
+    Battleship: {
+      horizontal:
+        "absolute text-ship-icon cursor-pointer w-full h-12 pointer-events-none animate-fade-in",
+      vertical:
+        "absolute text-ship-icon rotate-90 cursor-pointer w-48 h-12 pointer-events-none animate-fade-in",
+      component: (
+        <Battleship
+          className={
+            ship.isSunk ? shipIconStyles.isSunk : shipIconStyles.isFloat
+          }
+        />
+      ),
+    },
+    Cruiser: {
+      horizontal:
+        "absolute cursor-pointer w-full h-12 pointer-events-none animate-fade-in",
+      vertical:
+        "absolute rotate-90 cursor-pointer w-36 h-12 pointer-events-none animate-fade-in",
+      component: (
+        <Cruiser
+          className={
+            ship.isSunk ? shipIconStyles.isSunk : shipIconStyles.isFloat
+          }
+        />
+      ),
+    },
+    Submarine: {
+      horizontal:
+        "absolute cursor-pointer w-full h-12 pointer-events-none animate-fade-in",
+      vertical:
+        "absolute rotate-90 cursor-pointer w-36 h-12 pointer-events-none animate-fade-in",
+      component: (
+        <Submarine
+          className={
+            ship.isSunk ? shipIconStyles.isSunk : shipIconStyles.isFloat
+          }
+        />
+      ),
+    },
+    Destroyer: {
+      horizontal:
+        "absolute cursor-pointer w-full h-12 pointer-events-none animate-fade-in",
+      vertical:
+        "absolute rotate-90 cursor-pointer w-24 h-12 pointer-events-none animate-fade-in",
+      component: (
+        <Destroyer
+          className={
+            ship.isSunk ? shipIconStyles.isSunk : shipIconStyles.isFloat
+          }
+        />
+      ),
+    },
+  };
+
   return (
     <div className={shipIcons[ship.name][position]}>
       {shipIcons[ship.name].component}
